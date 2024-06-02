28905c4c1ce5194cc0e0bf81705608d2e6bc0b67
pythonmaker7 <arismastrok@gmail.com>
2024-06-02 14:34:55 +0100

    Fix import Path in settings.py

--- fumioxyz/settings.py
+++ fumioxyz/settings.py
@@ -5,6 +5,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -209,6 +211,8 @@ DEPLOY DETTINGS
 DEPLOY DETTINGS
 DEPLOY DETTINGS
 '''
+
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
