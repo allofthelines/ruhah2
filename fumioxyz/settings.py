059680f8bb5c868508a6e30aed4923ace1efe686
pythonmaker7 <arismastrok@gmail.com>
2024-06-02 15:12:45 +0100

    Ensure AWS S3 is properly configured for static and media files

--- fumioxyz/settings.py
+++ fumioxyz/settings.py
@@ -247,10 +247,13 @@ AWS_DEFAULT_ACL = None
 # AWS S3 settings for storing static and media files
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
-AWS_S3_REGION_NAME = 'your-region'  # e.g. 'us-east-1'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_DEFAULT_ACL = None
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 # Static files storage using S3
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
