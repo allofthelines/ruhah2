55d925de3a6804ff27dd9733d2d27ac38d21a40b
pythonmaker7 <arismastrok@gmail.com>
2024-06-04 19:46:50 +0100

    add edit button se profile auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', next_page='/'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', next_page='core:home'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
