1ffacd1a75e557d4909e24d4d43f8b349ac4b66e
pythonmaker7 <arismastrok@gmail.com>
2024-06-05 21:06:40 +0100

    Update project references from fumioxyz to ruhah

--- ruhah/asgi.py
+++ ruhah/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fummioxyz project.
+ASGI config for ruhah project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fumioxyz.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ruhah.settings")
 
 application = get_asgi_application()
