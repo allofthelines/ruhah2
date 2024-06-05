2aac57959ea6684ad9aeb04667058a1a731cbca5
pythonmaker7 <arismastrok@gmail.com>
2024-06-05 21:01:36 +0100

    Rename project to ruhah and update configurations

--- ruhah/wsgi.py
+++ ruhah/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fumioxyz project.
+WSGI config for ruhah project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fumioxyz.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ruhah.settings")
 
 application = get_wsgi_application()
