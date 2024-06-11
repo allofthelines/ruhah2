2eb631642fa8678b40c38dfcd46132722ed17fd1
pythonmaker7 <arismastrok@gmail.com>
2024-06-11 02:31:00 +0100

    Add signals to create Customer instance when CustomUser is created

---
+++ accounts/__init__.py
@@ -0,0 +1,3 @@
+# Ensure that the __init__.py file in the accounts app directory registers the AppConfig
+# to evala gia to signal pou ftiaxnei to customer aftomata meta to customuser
+default_app_config = 'accounts.AccountsConfig'
