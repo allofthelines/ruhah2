d8b7bbcb2382f0ba2904002cacc858c80efbad04
pythonmaker7 <arismastrok@gmail.com>
2024-06-11 23:05:37 +0100

    change in profile grid

--- core/admin.py
+++ core/admin.py
@@ -13,6 +13,13 @@ class OutfitAdmin(admin.ModelAdmin):
         return ""
     thumbnail.short_description = 'Image'
 
+    def portrait_thumbnail(self, obj):
+        if obj.portrait:
+            return format_html('<img src="{}" style="width: 50px; height: 50px;" />', obj.portrait.url)
+        return ""
+
+    portrait_thumbnail.short_description = 'Portrait'
+
 
 # Register the Outfit model with the OutfitAdmin options
 admin.site.register(Outfit, OutfitAdmin)
