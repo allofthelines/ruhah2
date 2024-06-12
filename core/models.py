83c3733c5d1e9b341319319a67bcdf7c8aa84d8f
pythonmaker7 <arismastrok@gmail.com>
2024-06-12 05:07:28 +0100

    change in profile grid

--- core/models.py
+++ core/models.py
@@ -12,7 +12,80 @@ from django.utils import timezone
 from django.apps import apps
 from django.conf import settings
 
+def get_image_upload_path(instance, filename):
+    return os.path.join('outfits/', filename)
 
+def get_portrait_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"portrait_{instance.id}.{ext}"
+    return os.path.join('portraits/', filename)
+
+class Outfit(models.Model):
+    rating = models.IntegerField(default=1000)
+    image = models.ImageField(upload_to=get_image_upload_path, default="outfits/default_img.jpg")
+    portrait = models.ImageField(upload_to=get_portrait_upload_path, default="portraits/default_img.jpg")
+    ticket_id = models.ForeignKey('box.Ticket', on_delete=models.SET_NULL, null=True, blank=True)
+    maker_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+    items = models.ManyToManyField('studio.Item', blank=True)
+
+    def __str__(self):
+        return f"pk={self.pk}, rating={self.rating}"
+
+    @property
+    def rank(self):
+        return Outfit.objects.aggregate(rank=Count("rating", filter=Q(rating__gt=self.rating), distinct=True) + 1)["rank"]
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            # Save instance first to get the primary key (pk)
+            super().save(*args, **kwargs)
+            # Reassign the portrait upload path with the ID now available
+            self.portrait.name = get_portrait_upload_path(self, self.portrait.name)
+        super().save(*args, **kwargs)
+        self._resize_image(500, 500)
+        self._resize_portrait(500, 500)
+
+    def _resize_image(self, width, height):
+        if self.image.width < width and self.image.height < height:
+            return
+
+        with self.image.open() as f:
+            image = Image.open(f)
+            image.load()
+
+        if image.width > width:
+            aspect_ratio = image.width / image.height
+            image = image.resize((width, round(width / aspect_ratio)))
+
+        if image.height > height:
+            image = image.crop((0, image.height - height, width, image.height))
+
+        with self.image.open("wb") as f:
+            image.save(f, 'JPEG')
+
+    def _resize_portrait(self, width, height):
+        if self.portrait.width < width and self.portrait.height < height:
+            return
+
+        with self.portrait.open() as f:
+            portrait = Image.open(f)
+            portrait.load()
+
+        if portrait.width > width:
+            aspect_ratio = portrait.width / portrait.height
+            portrait = portrait.resize((width, round(width / aspect_ratio)))
+
+        if portrait.height > height:
+            portrait = portrait.crop((0, portrait.height - height, width, portrait.height))
+
+        with self.portrait.open("wb") as f:
+            portrait.save(f, 'JPEG')
+
+    class Meta:
+        db_table = 'outfit_table'
+
+'''
 class Outfit(models.Model):
     rating = models.IntegerField(default=1000)
     image = models.ImageField(upload_to="outfits/", default="outfits/default_img.jpg")
@@ -72,3 +145,4 @@ class Outfit(models.Model):
 
     class Meta:
         db_table = 'outfit_table'
+'''
