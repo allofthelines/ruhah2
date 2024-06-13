c5a89b1abe0a66e471e3f1aaa7fd6095c04a021e
pythonmaker7 <arismastrok@gmail.com>
2024-06-13 10:03:38 +0100

    Ensure Celery task registration and add debug statements

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -2,13 +2,14 @@
 
 from celery import shared_task
 
+
+@shared_task(name='accounts.tasks.test_task')
+def test_task():
+    print("Test task executed")
+
 @shared_task
 def clear_user_item_cart():
     from .models import UserItemCart
     UserItemCart.objects.all().delete()
 
 print("Loading tasks from accounts...")
-
-@shared_task(name='accounts.tasks.test_task')
-def test_task():
-    print("Test task executed")
