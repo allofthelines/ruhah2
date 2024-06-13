6406c2e1c8e9482ccf4f6a1e841353a78d8e7ff5
pythonmaker7 <arismastrok@gmail.com>
2024-06-13 09:03:19 +0100

    Ensure Celery task discovery and add debug statements

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -5,6 +5,8 @@ def clear_user_item_cart():
     from .models import UserItemCart
     UserItemCart.objects.all().delete()
 
+print("Loading tasks from accounts...")
+
 @shared_task
 def test_task():
     print("Test task executed")
--- ruhah/celery.py
+++ ruhah/celery.py
@@ -1,3 +1,5 @@
+# ruhah/celery.py
+
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
@@ -8,4 +10,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ruhah.settings')
 app = Celery('ruhah')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+print("Autodiscovering tasks...")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+print("Autodiscovered tasks!")
