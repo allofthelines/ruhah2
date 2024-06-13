89e222e80d0c34ca7eaa48caafb8fad6da3c6765
pythonmaker7 <arismastrok@gmail.com>
2024-06-13 08:46:41 +0100

    celery

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -4,3 +4,7 @@ from celery import shared_task
 def clear_user_item_cart():
     from .models import UserItemCart
     UserItemCart.objects.all().delete()
+
+@shared_task
+def test_task():
+    print("Test task executed")
