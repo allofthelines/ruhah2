d831a733b63076526a479a751d7cf16d55196393
pythonmaker7 <arismastrok@gmail.com>
2024-06-15 06:59:43 +0100

    ecommerce stores

--- studio/management/commands/test_shopify.py
+++ studio/management/commands/test_shopify.py
@@ -9,11 +9,11 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         api_key = os.getenv('SHOPIFY_API_KEY_DEV')
         api_secret = os.getenv('SHOPIFY_API_SECRET_DEV')
-        access_token = os.getenv('SHOPIFY_ACCESS_TOKEN_DEV')
+        api_access_token = os.getenv('SHOPIFY_ACCESS_TOKEN_DEV')
         shop_url = f"https://{api_key}:{api_secret}@fumioxyz.myshopify.com/admin"
 
         shopify.ShopifyResource.set_site(shop_url)
-        session = shopify.Session(shop_url, version="2023-04", token=access_token)
+        session = shopify.Session(shop_url, version="2023-04", token=api_access_token)
         shopify.ShopifyResource.activate_session(session)
 
         # Fetch Products from Shopify
@@ -34,15 +34,15 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         api_key = os.getenv('SHOPIFY_API_KEY_DEV')
         api_secret = os.getenv('SHOPIFY_API_SECRET_DEV')
-        access_token = os.getenv('SHOPIFY_ACCESS_TOKEN_DEV')
+        api_access_token = os.getenv('SHOPIFY_ACCESS_TOKEN_DEV')
         
-        if not all([api_key, api_secret, access_token]):
+        if not all([api_key, api_secret, api_access_token]):
             self.stderr.write(self.style.ERROR('One or more Shopify API credentials are missing.'))
             return
         
         shop_url = f"https://{api_key}:{api_secret}@fumioxyz.myshopify.com/admin"
         shopify.ShopifyResource.set_site(shop_url)
-        session = shopify.Session(shop_url, version="2023-04", token=access_token)
+        session = shopify.Session(shop_url, version="2023-04", token=api_access_token)
         shopify.ShopifyResource.activate_session(session)
         
         try:
