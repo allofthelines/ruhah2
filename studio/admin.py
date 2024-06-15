906a066c73cdf66cd7a657aef421b59ac6ded1bc
pythonmaker7 <arismastrok@gmail.com>
2024-06-15 01:39:14 +0100

    taglist typo admin

--- studio/admin.py
+++ studio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, Tag, StudioOutfitTemp, ShopifyStore, SizeCategory
+from .models import Item, Tag, StudioOutfitTemp, ShopifyStore, SizeCategory, SizeShoeUkCategory, SizeWaistInchCategory
 from django.utils.html import format_html
 from django.contrib.admin import SimpleListFilter
 from .forms import ShopifyStoreForm
