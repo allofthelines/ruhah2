b056d749fc0a30fe5d02ff14b995763a685bbc1e
pythonmaker7 <arismastrok@gmail.com>
2024-06-16 07:51:56 +0100

    changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -25,7 +25,7 @@ class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'name', 'bio', 'pfp', 'profile_visibility', 'credits']
+        fields = ['username', 'email', 'name', 'bio', 'pfp']
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user', None)
@@ -36,8 +36,6 @@ class UserProfileForm(forms.ModelForm):
             self.fields['name'].initial = user.name
             self.fields['bio'].initial = user.bio
             self.fields['pfp'].initial = user.pfp
-            self.fields['profile_visibility'].initial = user.profile_visibility
-            self.fields['credits'].initial = user.credits
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -51,8 +49,6 @@ class UserProfileForm(forms.ModelForm):
         user.username = self.cleaned_data['username']
         user.name = self.cleaned_data['name']
         user.bio = self.cleaned_data['bio']
-        user.profile_visibility = self.cleaned_data['profile_visibility']
-        user.credits = self.cleaned_data['credits']
         if 'pfp' in self.cleaned_data:
             user.pfp = self.cleaned_data['pfp']
 
