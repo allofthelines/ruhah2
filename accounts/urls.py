00c421509b5de9bf23147af05012e460fe4c61e3
pythonmaker7 <arismastrok@gmail.com>
2024-06-17 04:15:07 +0100

    changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('unfollow/<str:username>/', views.unfollow, name='unfollow'),
     path('<str:username>/followers/', views.followers_list, name='followers_list'),
     path('<str:username>/following/', views.following_list, name='following_list'),
+
+    path('like-item/<int:item_id>/', views.like_item, name='like_item'),
 ]
