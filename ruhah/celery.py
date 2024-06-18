2684bbdfec270aaa53636fe919e12cfd23da8d49
pythonmaker7 <arismastrok@gmail.com>
2024-06-18 10:04:13 +0100

    changes

--- ruhah/celery.py
+++ ruhah/celery.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
+"""from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.conf import settings
@@ -12,5 +12,28 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f'Request: {self.request!r}')"""
+
+# celery.py
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ruhah.settings')
+
+app = Celery('ruhah')
+
+if getattr(settings, 'CELERY_ENABLED', True):
+    app.config_from_object('django.conf:settings', namespace='CELERY')
+    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+    @app.task(bind=True)
+    def debug_task(self):
+        print(f'Request: {self.request!r}')
+else:
+    @app.task(bind=True)
+    def debug_task(self):
+        print('Celery is disabled')
 
