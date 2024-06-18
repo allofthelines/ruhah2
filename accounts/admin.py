5741f599ef933ee9d4ba7a49a0678b9426eba7fc
pythonmaker7 <arismastrok@gmail.com>
2024-06-18 01:03:54 +0100

    changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,10 +9,10 @@ class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'id', 'email', 'is_superuser']
 
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('is_stylist', 'name', 'bio', 'pfp', 'profile_visibility', 'trending_mode', 'trending_styles', 'credits')}),
+        (None, {'fields': ('is_stylist', 'name', 'bio', 'pfp', 'profile_visibility', 'trending_mode', 'trending_styles', 'studio_styles', 'credits')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('is_stylist', 'name', 'bio', 'pfp', 'profile_visibility', 'trending_mode', 'trending_styles', 'credits')}),
+        (None, {'fields': ('is_stylist', 'name', 'bio', 'pfp', 'profile_visibility', 'trending_mode', 'trending_styles', 'studio_styles', 'credits')}),
     )
 admin.site.register(CustomUser, CustomUserAdmin)
 
