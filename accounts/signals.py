446aa17336646bf044b15a20689fa04e27cee39c
pythonmaker7 <arismastrok@gmail.com>
2024-06-19 10:34:28 +0100

    changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -18,15 +18,13 @@ def create_customer(sender, instance, created, **kwargs):
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.apps import apps
-from .models import CustomUser
 
 print('\n\n\n\n\n\n', '1111111111111', '\n\n\n\n\n\n')
+@receiver(post_save, sender='accounts.CustomUser')
 def set_default_styles(sender, instance, created, **kwargs):
     print('\n\n\n\n\n\n', '222222222222', '\n\n\n\n\n\n')
     if created:
-        print('\n\n\n\n\n\n', '33333333333', '\n\n\n\n\n\n')
         Style = apps.get_model('studio', 'Style')
         all_styles = Style.objects.all()
         instance.trending_styles.set(all_styles)
         instance.studio_styles.set(all_styles)
-        print('\n\n\n\n\n\n', '44444444444', '\n\n\n\n\n\n')
