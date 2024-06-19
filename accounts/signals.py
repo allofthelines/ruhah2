e1592e0516611dc13d4cb62c0ef02e37fa99c89b
pythonmaker7 <arismastrok@gmail.com>
2024-06-19 11:29:09 +0100

    changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -16,28 +16,24 @@ def create_customer(sender, instance, created, **kwargs):
 
 
 from django.db.models.signals import post_save
+
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from django.apps import apps
 from .models import CustomUser
 
-
-@receiver(post_save, sender='accounts.CustomUser')
-def set_default_styles(sender, instance, created, **kwargs):
-    if created:
+@receiver(m2m_changed, sender=CustomUser.trending_styles.through)
+def set_default_styles(sender, instance, action, **kwargs):
+    if action == 'post_add':
         Style = apps.get_model('studio', 'Style')
         all_styles = Style.objects.all()
 
-        # Save the instance before setting ManyToMany fields
-        instance.save()
-
-        # Now set the ManyToMany fields
+        # Set the ManyToMany fields
         instance.trending_styles.set(all_styles)
         instance.studio_styles.set(all_styles)
-        instance.bio = 'penis'
 
         # Debugging output
-        print('\n\n\n\n\n\nALL_STYLES:', all_styles)
-        print('\n\n\n\n\n\ninstance.trending_styles:', type(instance.trending_styles.all()))
-        print('\n\n\n\n\n\ninstance.studio_styles:', instance.studio_styles.all())
-        print('\n\n\n\n\n\ninstance.username:', instance.username)
-        print('\n\n\n\n\n\ninstance.bio:', instance.bio)
+        print('\n\n\n\n\nALL_STYLES:', all_styles)
+        print('\n\n\n\n\ninstance.trending_styles:', instance.trending_styles.all())
+        print('\n\n\n\n\ninstance.studio_styles:', instance.studio_styles.all())
+
