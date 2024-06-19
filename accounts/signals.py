93a7311ffa232b48e6ebf8cc4567ec902c4cc940
pythonmaker7 <arismastrok@gmail.com>
2024-06-19 10:46:29 +0100

    changes

--- accounts/signals.py
+++ accounts/signals.py
@@ -25,7 +25,9 @@ print('\n\n\n\n\n\n', '1111111111111', '\n\n\n\n\n\n')
 def set_default_styles(sender, instance, created, **kwargs):
     print('\n\n\n\n\n\n', '222222222222', '\n\n\n\n\n\n')
     if created:
+        print('\n\n\n\n\n\n', '33333333333', '\n\n\n\n\n\n')
         Style = apps.get_model('studio', 'Style')
         all_styles = Style.objects.all()
         instance.trending_styles.set(all_styles)
         instance.studio_styles.set(all_styles)
+        print('\n\n\n\n\n\n', '444444444444', '\n\n\n\n\n\n')
