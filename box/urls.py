240383d46cb5b940ce18f8a510d20ffa35d51f36
pythonmaker7 <arismastrok@gmail.com>
2024-06-30 22:22:13 +0100

    changes

--- box/urls.py
+++ box/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ticket_view, success_view, api_tickets
 from .views import ticket_view, success_view, api_tickets, stripe_webhook, create_checkout_session
 from .views import payment_successful, payment_rejected
-from .views import ask_fit_view, ask_box_view, ask_fit_success, success_view
+from .views import ask_fit_success, success_view
 
 urlpatterns = [
     path('ticket/', ticket_view, name='ticket_form'),
