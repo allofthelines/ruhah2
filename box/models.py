f66ba659ab48ffa8f3e9efa47695af293736d260
pythonmaker7 <arismastrok@gmail.com>
2024-07-15 20:27:58 +0100

    changes

--- box/models.py
+++ box/models.py
@@ -26,11 +26,12 @@ class Ticket(models.Model):
         ('closed', 'closed'),
         ('boxed', 'boxed'),
     ]
-
+    """
     TYPE_CHOICES = [
         ('styled_outfits', 'styled outfits'),
         ('liked_items', 'liked items'),
     ]
+    """
 
     ASKTYPE_CHOICES = [
         ('outfit', 'outfit'),
@@ -43,7 +44,7 @@ class Ticket(models.Model):
     ]
 
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='notpaid')
-    type = models.CharField(max_length=30, choices=TYPE_CHOICES, default='styled_outfits')
+    # type = models.CharField(max_length=30, choices=TYPE_CHOICES, default='styled_outfits')
     asktype = models.CharField(max_length=30, choices=ASKTYPE_CHOICES, default='outfit')
     filter_liked = models.CharField(max_length=30, choices=FILTER_LIKED_CHOICES, default='no_filter')
     timestamp = models.DateTimeField(default=timezone.now)
