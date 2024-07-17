467ae9249cee419fa2a3f1a62671a21d7cde8e66
pythonmaker7 <arismastrok@gmail.com>
2024-07-17 10:37:56 +0100

    changes

--- studio/management/commands/01_PIPELINES.py
+++ studio/management/commands/01_PIPELINES.py
@@ -2,18 +2,27 @@
 
 NEW IMAGE UPLOAD PIPELINE se S3 KAI POSTGRES (BULK)
 
-1. raw fwtografies se /media/items-temp
+1.
+raw fwtografies se /media/items-temp
 
-2. REMOVE BACKGROUND
-3. python manage.py crop_png
-4. python manage.py square_png
+2.
+REMOVE BACKGROUND
+3.
+python manage.py crop_png
+4.
+python manage.py square_png
 
-5. manually rename 1.png 2.png
-6. manually update /studio/static/studio/new_items.json
+5.
+manually rename 1.png 2.png
+6.
+manually update /studio/static/studio/new_items.json
 
-7. python manage.py rename_items_before_upload
-8. python manage.py upload_items_s3
-9. heroku run python manage.py upload_items_json
+7.
+python manage.py rename_items_before_upload
+8.
+python manage.py upload_items_s3
+9.
+heroku run python manage.py upload_items_json
 
 
 """
