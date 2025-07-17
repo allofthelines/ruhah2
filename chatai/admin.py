403c1dbdc7bd0ecec83bb4af08b76b6604380fc9
pythonmaker7 <arismastrok@gmail.com>
2025-07-17 20:25:31 +0300

    changes

--- chatai/admin.py
+++ chatai/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_link', 'product_price', 'product_details', 'product_created_at')  # Columns in list view
+    fields = ('product_name', 'product_link', 'product_images', 'product_price', 'product_details')  # Editable in detail view (JSON as text)
+    readonly_fields = ('product_created_at',)  # Auto-field non-editable
