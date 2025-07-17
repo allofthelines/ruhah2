575696780325824529c129e01f72c3ded085934a
pythonmaker7 <arismastrok@gmail.com>
2025-07-17 20:22:55 +0300

    changes

--- chatai/models.py
+++ chatai/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_link = models.URLField()
+    product_images = models.JSONField()  # Stores list of dicts like [{'url': 'https://...', 'desc': 'Image 0'}]
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_details = models.TextField()
+    product_created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
