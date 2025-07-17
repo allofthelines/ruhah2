b1b96c2f20f1191693312550c94bad96d8f3aa9e
pythonmaker7 <arismastrok@gmail.com>
2025-07-17 20:54:51 +0300

    changes

--- chatai/admin.py
+++ chatai/admin.py
@@ -3,6 +3,6 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_link', 'product_price', 'product_details', 'product_created_at')  # Columns in list view
-    fields = ('product_name', 'product_link', 'product_images', 'product_price', 'product_details')  # Editable in detail view (JSON as text)
+    list_display = ('product_name', 'product_brand', 'product_link', 'product_price', 'product_details', 'product_created_at')  # Columns in list view
+    fields = ('product_name', 'product_brand', 'product_link', 'product_images', 'product_price', 'product_details')  # Editable in detail view (JSON as text)
     readonly_fields = ('product_created_at',)  # Auto-field non-editable
