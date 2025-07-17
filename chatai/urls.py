92166fb0c14d04a4100a2d225be138b627d19a2e
pythonmaker7 <arismastrok@gmail.com>
2025-07-18 01:25:18 +0300

    changes

--- chatai/urls.py
+++ chatai/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'chatai'
+
 urlpatterns = [
     path('start-aichat/', views.AIChatStartView.as_view(), name='start_aichat'),  # Starter for button
     path('aichat/', views.AIChatView.as_view(), name='aichat'),  # Main clean URL
