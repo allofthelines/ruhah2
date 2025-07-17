cb5a4584f78ca217d0db590ed1ea4c4319adb567
pythonmaker7 <arismastrok@gmail.com>
2025-07-17 21:34:24 +0300

    changes

--- chatai/models.py
+++ chatai/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pgvector.django import VectorField
 
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
@@ -9,6 +10,8 @@ class Product(models.Model):
     product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_details = models.TextField()
     product_created_at = models.DateTimeField(auto_now_add=True)
+
+    product_embedding = VectorField(dimensions=768, blank=True, null=True)
 
     def __str__(self):
         return self.product_name
