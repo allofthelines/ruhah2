038630529c103f1522a4a78b2ec40ed6d395c2cf
pythonmaker7 <arismastrok@gmail.com>
2025-07-18 16:35:43 +0300

    changes

--- chatai/admin.py
+++ chatai/admin.py
@@ -5,7 +5,7 @@ from .models import Product, ChatSession, ChatMessage
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'product_brand', 'product_link', 'product_price', 'product_details', 'product_created_at')  # Columns in list view
 
-    fields = ('product_name', 'product_brand', 'product_link', 'product_images', 'product_main_image' 'product_price', 'product_details')  # Editable in detail view (JSON as text)
+    fields = ('product_name', 'product_brand', 'product_link', 'product_images', 'product_main_image', 'product_price', 'product_details')  # Editable in detail view (JSON as text)
     readonly_fields = ('product_created_at', 'product_embedding',)  # Auto-field non-editable
 
 class ChatSessionAdmin(admin.ModelAdmin):
@@ -14,10 +14,12 @@ class ChatSessionAdmin(admin.ModelAdmin):
     fields = ('chat_id', 'chat_user', 'chat_reference_item', 'chat_reference_outfit_id', 'chat_status')
     readonly_fields = ('chat_main_embedding', 'chat_created_at')
 
+admin.site.register(ChatSession, ChatSessionAdmin)
 
 class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ('msg_chat_session', 'msg_is_from_user', 'msg_text', 'msg_message_type')
 
     fields = ('msg_chat_session', 'msg_is_from_user', 'msg_text', 'msg_message_type', 'msg_recommendations', 'msg_image', 'msg_image_url')
-    readonly_fields = ('msg_created_at')
+    readonly_fields = ('msg_created_at',)
 
+admin.site.register(ChatMessage, ChatMessageAdmin)
