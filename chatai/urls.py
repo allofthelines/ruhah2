82682456b79cce799e3ea0e352fc423095509486
pythonmaker7 <arismastrok@gmail.com>
2025-07-18 12:09:11 +0300

    changes

--- chatai/urls.py
+++ chatai/urls.py
@@ -7,4 +7,3 @@ urlpatterns = [
     path('aichat/', views.AIChatStartView.as_view(), name='start_aichat'),  # Starter for button
     path('aichat/<str:chat_id>/', views.AIChatView.as_view(), name='aichat'),
 ]
-]
