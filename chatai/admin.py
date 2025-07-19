e922898fa2a76ce1eff724fc5670d8dff25b79ca
pythonmaker7 <arismastrok@gmail.com>
2025-07-19 03:16:42 +0300

    changes

--- chatai/admin.py
+++ chatai/admin.py
@@ -3,10 +3,19 @@ from .models import Product, ChatSession, ChatMessage
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'product_brand', 'product_link', 'product_price', 'product_details', 'product_created_at')  # Columns in list view
+    list_display = ('product_name', 'product_brand', 'product_link', 'product_price', 'product_details', 'product_created_at', 'has_embedding')  # Columns in list view
 
     fields = ('product_name', 'product_brand', 'product_link', 'product_images', 'product_main_image', 'product_price', 'product_details', 'product_created_at', 'product_embedding')  # Editable in detail view (JSON as text)
     readonly_fields = ('product_created_at', 'product_embedding',)  # Auto-field non-editable
+
+    readonly_fields = ('product_created_at', 'product_embedding',)  # Auto-field non-editable
+
+    def has_embedding(self, obj):
+        # Returns "Yes" if product_embedding is populated (truthy), else "No"
+        return "Yes" if obj.product_embedding else "No"
+
+    has_embedding.short_description = "Has Embedding"  # Column header in admin
+    has_embedding.boolean = True
 
 class ChatSessionAdmin(admin.ModelAdmin):
     list_display = ('chat_id', 'chat_user', 'chat_reference_item', 'chat_reference_outfit_id', 'chat_status', 'chat_created_at')
