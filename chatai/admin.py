ab0fd816b1c0375a3e1c5679b45f05e333e5ae6e
pythonmaker7 <arismastrok@gmail.com>
2025-07-19 03:48:52 +0300

    changes

--- chatai/admin.py
+++ chatai/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product, ChatSession, ChatMessage
 
 
-@admin.register(Product)
+# @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'product_name',
